996f2fc7e9c65fb05668e7c670d82fa30e45ac01
Pierpaolo Gorelli <pierpaologorelli@MacBook-Pro-di-Pierpaolo.local>
2016-04-28 14:32:26 +0200

    added spanish language

---
+++ config/deploy/staging.rb
@@ -0,0 +1,30 @@
+set :stage, :staging
+
+# Simple Role Syntax
+# ==================
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+server '178.62.201.7', user: 'serverpilot', roles: %w{web app db}
+set :deploy_to, -> { "/srv/users/serverpilot/apps/#{fetch(:application)}/deploy" }
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(~/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+
+fetch(:default_env).merge!(wp_env: :staging)
+
+set :wpcli_remote_url, -> {"http://cafetin.bits2beats.com"}
+set :wpcli_local_url, -> {"http://cafetin.dev/"}
+
+set :wpcli_local_uploads_dir, -> {"/Users/pierpaologorelli/Documents/Websites/cafetin.es/web/app/uploads/"}
+set :wpcli_remote_uploads_dir, -> {"/srv/users/serverpilot/apps/#{fetch(:application)}/deploy/shared/web/app/uploads/"}
